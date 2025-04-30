fbc68bc971e85cc902e549ed2fc3a1f89a6bacae
Juan Pablo Uribe Gomez <uribegomezjuanpablo@gmail.com>
2025-04-30 02:44:46 -0500

    update todos-api cors

--- todos-api/server.js
+++ todos-api/server.js
@@ -3,6 +3,15 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const jwt = require('express-jwt')
 
+const cors = require('cors');
+
+// Configura CORS antes de usar jwt o tus rutas
+app.use(cors({
+  origin: '*', // o especifica tu frontend: 'http://54.226.22.120:8080'
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 const ZIPKIN_URL = process.env.ZIPKIN_URL || 'http://127.0.0.1:9411/api/v2/spans';
 const {Tracer, 
   BatchRecorder,
